b4182968c4eeb182099ad0f8f24a240368283f17
Landgraf Paul <landgraf.paul@gmail.com>
2018-06-21 12:26:07 +0200

    added config js

--- src/app.js
+++ src/app.js
@@ -14,6 +14,10 @@ class Core extends EventEmitter {
 	}
 
 	async initialize() {
+		await Promise.all(require('./config.js').map(async (module) => {
+			return await module.call(this).catch(console.error)
+		}));
+
 		await Promise.all(require('./plugins').map(async (module) => {
 			return await module.call(this).catch(console.error)
 		}));
---
+++ src/config.js
@@ -0,0 +1,3 @@
+module.exports = function () {
+	this.config.set("project.name", "seed and nancy")
+}
