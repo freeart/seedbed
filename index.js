4c915bd91331cf1b0c5426ac4267ba262546c1a0
Landgraf Paul <landgraf.paul@gmail.com>
2018-06-21 13:37:55 +0200

    module implement

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-require("./src")
+const EventEmitter = require('events');
+
+class Core extends EventEmitter {
+	constructor(env) {
+		super();
+
+		this.config = require('nodejs-config')(
+			__dirname,
+			function()
+			{
+				return process.env.NODE_ENV;
+			}
+		);
+
+		this.env = env;
+	}
+
+	async initialize() {
+		await Promise.all(this.env.config.map(async (module) => {
+			return await module.call(this).catch(console.error)
+		}));
+
+		await Promise.all(this.env.plugins.map(async (module) => {
+			return await module.call(this).catch(console.error)
+		}));
+
+		await Promise.all(this.env.modules.map(async (module) => {
+			return await module.call(this).catch(console.error)
+		}));
+
+		this.emit("ready");
+	}
+
+	static async create() {
+		const o = new Core();
+		await o.initialize();
+		return o;
+	}
+}
+
+Core.create();
--- src/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = function () {
-	this.config.set("project.port", 8080)
-	this.config.set("project.www", path.join(__dirname, '../public/dist'))
-}
