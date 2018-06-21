42603a48c83a5e3739902f6568e5e466d4196f97
Landgraf Paul <landgraf.paul@gmail.com>
2018-06-21 14:45:56 +0200

    catch errors

--- index.js
+++ index.js
@@ -6,25 +6,28 @@ class Core extends EventEmitter {
 
 		this.config = require('nodejs-config')(
 			__dirname,
-			function()
-			{
+			function () {
 				return process.env.NODE_ENV;
 			}
 		);
 	}
 
 	async initialize() {
-		await Promise.all(Core.ENVIRONMENT.config.map(async (module) => {
-			return await module.call(this).catch(console.error)
-		}));
+		try {
+			await Promise.all(Core.ENVIRONMENT.config.map(async (module) => {
+				return await module.call(this).catch(console.error)
+			}));
 
-		await Promise.all(Core.ENVIRONMENT.plugins.map(async (module) => {
-			return await module.call(this).catch(console.error)
-		}));
+			await Promise.all(Core.ENVIRONMENT.plugins.map(async (module) => {
+				return await module.call(this).catch(console.error)
+			}));
 
-		await Promise.all(Core.ENVIRONMENT.modules.map(async (module) => {
-			return await module.call(this).catch(console.error)
-		}));
+			await Promise.all(Core.ENVIRONMENT.modules.map(async (module) => {
+				return await module.call(this).catch(console.error)
+			}));
+		} catch (e) {
+			console.error(e)
+		}
 
 		this.emit("ready");
 	}
@@ -37,4 +40,4 @@ class Core extends EventEmitter {
 	}
 }
 
-module.exports =  Core.create;
+module.exports = Core.create;
